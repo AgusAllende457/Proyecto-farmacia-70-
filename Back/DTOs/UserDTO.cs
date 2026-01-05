7136982ab957d3477d6d68451a5bef117c6e6687
AgusAllende457 <agustina.allende.mansilla@mi.unc.edu.ar>
2026-01-05 17:03:55 -0300

    creacion de DTOs, Mappings y Validators

---
+++ Back/DTOs/UserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Back.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+
+        // Concatenación para reportes (Ref: RF5 - Generación de valor en reportes).
+        public string NombreCompleto { get; set; }
+        public string Email { get; set; }
+
+        // Ref: RF8 - El rol determina qué estados de pedido puede cambiar el usuario (RF1, RF1.1).
+        public string Rol { get; set; }
+
+        // Ref: Entidad Sucursal - "Permite segmentar la operación por ubicación".
+        public string NombreSucursal { get; set; }
+    }
+}
