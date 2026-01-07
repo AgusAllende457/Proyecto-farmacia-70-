f7686473c09ebe9500fa40176a3d0cf943a7962f
jermiasoliva0805 <jeremiasoliva0805@mi.unc.edu.ar>
2026-01-07 15:26:48 -0300

    nueva carpeta en Service

--- Back/obj/Debug/net8.0/Back.AssemblyInfo.cs
+++ Back/obj/Debug/net8.0/Back.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6402928cae012d07c60f400e458b46afe599c765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dad992fe7bb803e87e09de0d50baaee3e81f9ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
