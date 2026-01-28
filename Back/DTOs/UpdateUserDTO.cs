23bf75ad5c9c489394bf88ff33a211276fa96771
AgusAllende457 <agustina.allende.mansilla@mi.unc.edu.ar>
2026-01-28 17:34:17 -0300

    RegisterUserDTO y UpdateUserDTO

---
+++ Back/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Back.DTOs
+{
+    public class UpdateUserDTO
+    {
+        [Required]
+        public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "El nombre completo es obligatorio")]
+        public string NombreCompleto { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string DNI { get; set; }
+
+        public string Telefono { get; set; }
+
+        [Required]
+        public string Rol { get; set; }
+
+        [Required]
+        public int IdSucursal { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
